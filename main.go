2b51bcc08a7304f41b8e3403bcacbbff836f4191
tur1amant <tur1amant@gmail.com>
2021-03-18 00:33:42 +0300

    refactoring coda

--- main.go
+++ main.go
@@ -1,130 +1,137 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/mattn/go-sqlite3"
 	"os"
-	"io"
 )
 
 /*   ====== This is main ============   */
-func main(){
-	filename:= "slovar.dat"
 
-	var slovar map[string]string
-	slovar = make(map[string]string)
-	//var p_sl  *map[string]string
-	//p_sl = &slovar
+func main() {
 
-	fmt.Println("This is a program for learning English by word cards")
+	for {
+		fmt.Println("Eng-Ru Dictionary")
+		fmt.Println("Menu: ")
+		fmt.Println("0 - Create new Base Data(BD)")
+		fmt.Println("1 - Show all records from BD")
+		fmt.Println("111 - Show ЯЯЯall records from BD")
+		fmt.Println("2 - Add record into BD")
+		fmt.Println("3 - Edit record in BD")
+		fmt.Println("4 - Delete record")
+		fmt.Println("5 - Delete all records")
+		fmt.Println("6 - Quit")
 
-	var i int
-	for i != 9 {
-	    fmt.Println("Select <1> - to record <2> - to view")
-	    fmt.Scan(&i)
-            switch i {
+		var choice int
+		fmt.Println("Ваш выбор: ")
+		fmt.Scan(&choice)
+		switch choice {
 		case 0:
-		        createFileWords(filename)
-		        break
+			createBaseData()
+			break
 		case 1:
-			recordWords(slovar, filename)
+			showRecords()
 			break
 		case 2:
-			viewWords(slovar, filename)
+			recordDataBaseData()
 			break
 		case 3:
-		        randWords(filename)
-		        break	
+			updateEngSlovar()
+			break
+		case 4:
+			deleteEngSlovar()
+			break
+		case 5:
+			deleteAll()
+			break
+		case 6:
+			fmt.Println("Good bye!")
 		default:
-			fmt.Println("Default switch!")
+			fmt.Println("Нет такого выбора, повторите!")
+			break
 		}
+
 	}
-	fmt.Println("Good bye!!!")
+}
+
+func createBaseData(){
+	fmt.Println("createBasedata")
+	database, err := sql.Open("sqlite3", "./basedata.db")
+	if err != nil {
+		panic(err)
+	}
+	defer database.Close()
+	fmt.Println("CREATE TABLE ")
+	table, err := database.Exec("CREATE TABLE dictionary (eng text ,rus text)")
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(table)
+}
+
+func showRecords(){
+	fmt.Println("---====ZZZ showRecords ZZZ ====---")
+	database, err := sql.Open("sqlite3", "./basedata.db")
+	if err != nil {
+		panic(err)
+	}
+	defer database.Close()
+	rows, err := database.Query("SELECT * FROM dictionary")
+	if err != nil{
+		panic(err)
+	}
+	defer rows.Close()
+
+	var engl string
+	var rusl string
+
+	for rows.Next(){
+		err := rows.Scan(&engl, &rusl)
+		if err != nil{
+			fmt.Println(err)
+			continue
+		}
+		fmt.Println(engl + " - "+ rusl)
+		}
+		fmt.Println("===================")
+	}
+
+func recordDataBaseData() {
+	fmt.Println("recordDataBaseData")
+
+	var eng_L, rus_L string
+	fmt.Println("введите анг слово: ")
+	fmt.Fscan(os.Stdin, &eng_L)
+	fmt.Println("введите анг русское слово: ")
+	fmt.Fscan(os.Stdin, &rus_L)
+
+	database, err := sql.Open("sqlite3", "./basedata.db")
+	if err != nil {
+		panic(err)
+	}
+	defer database.Close()
+	//statement, err := database.Exec("INSERT INTO dictionary VALUES  ('write','писать')")
+	statement, err := database.Prepare("INSERT INTO dictionary (eng, rus) VALUES (?, ?)")
+	if err != nil {
+		panic(err)
+	}
+	statement.Exec(eng_L, rus_L)
+	//statement, err := database.Exec("INSERT INTO dictionary (eng, rus) VALUES  ('write','писать')")
+	//fmt.Println(statement.LastInsertId())
+	//fmt.Println(statement.RowsAffected())
 
 }
 
-/* File creation function*/
-func createFileWords(filename string){
-    file ,err := os.Create(filename)
-	if err != nil {
-	    fmt.Println(err)
-	    os.Exit(1)
-	} 
-	defer file.Close()
+func updateEngSlovar(){
+	fmt.Println("updateEngSlovar")
+}
+ func deleteEngSlovar() {
+	 fmt.Println("deleteEngSlovar")
 }
 
-/*Random word generator function*/
-func randWords(filename string){
-	var engword string
-	var rusword string
-	
-	var slov_rand map[string]string
-	slov_rand = make(map[string]string)
-
-	file, err := os.Open(filename)
-	if err != nil {
-	    fmt.Println(err)
-	    os.Exit(1)
-	}
-	defer file.Close()
-	
-	for{
-	    _, err = fmt.Fscanf(file,"%s\t%s\n", &engword, &rusword )
-	    if err != nil{
-	        if err == io.EOF{
-	            break
-	            }else{
-	            fmt.Println(err)
-	            os.Exit(1)
-	            }
-	        }
-	        slov_rand[engword]=rusword
-	    }
-	} 
-/* Writing words to a file */
-func recordWords(sl map[string]string, filename string){
-        var engword, rusword string
-
-	fmt.Println("Enter a word in English")
-	fmt.Scan(&engword)
-
-	fmt.Println("Enter translation in Russian")
-	fmt.Scan(&rusword)
-
-	sl[engword]=rusword
-	
-	file, err := os.OpenFile(filename, os.O_RDWR,0660)
-	if err != nil {
-	    fmt.Println(err)
-	    os.Exit(1)
-	}
-	defer file.Close()
-	
-	for k, v := range sl{
-	    fmt.Println("I write to file")
-	    fmt.Fprintf(file, "%s\t%s\n", k, v)
-	}
+func deleteAll(){
+	fmt.Println("deleteAll")
 }
-/* View all words from a file */
-func viewWords(sl map[string]string, filename string){
-	var engword string
-	var rusword string
-	file, err := os.OpenFile(filename, os.O_RDONLY, 0666) 
-	if err != nil {
-	    fmt.Println(err)
-	    os.Exit(1)
-	}
-	defer file.Close()
-	for{
-	    _, err = fmt.Fscanf(file,"%s\t%s\n", &engword, &rusword )
-	    if err != nil{
-	        if err == io.EOF{
-	            break
-	            }else{
-	            fmt.Println(err)
-	            os.Exit(1)
-	            }
-	        }
-	        fmt.Printf("%s\t%s\n", engword, rusword)
-	    }
-	} 
 
